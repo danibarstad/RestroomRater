6482bb467b77133019cdbdaccedd47d84e921d7b
Dani <danibarstad@gmail.com>
2020-04-24 13:29:51 -0500

    made first Yelp API call

--- yelp.py
+++ yelp.py
@@ -1,12 +1,17 @@
 import os
 import requests
 
+loc = 'Georgetown'  # TODO: change to get location as user input
 enVar = os.environ.get('YELP_API_KEY')
-url = 'https://api.yelp.com/v3/businesses/'
-params = {'id':'WavvLdfdP6g8aZTtbBQHTw'}
-headers = {'Authorization': f'Bearer {enVar}'}
+business_id = 'WavvLdfdP6g8aZTtbBQHTw'
+url = 'https://api.yelp.com/v3/businesses/search'
+params = {
+    'location': f'{loc}',
+    'limit': 1
+}
+headers = {'Authorization': 'Bearer %s' % enVar}
 
 data = requests.get(url, params=params, headers=headers).json()
-print(data)
-# name = data['name']
-# print(f'The name of the establishment is: {name}')
+# print(data)
+name = data['businesses'][0]['name']
+print(f'The name of the establishment is: {name}')
