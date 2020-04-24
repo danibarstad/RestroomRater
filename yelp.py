1a892c96cc298183f3e9629e04eb5ff1d59b4afa
Dani <danibarstad@gmail.com>
2020-04-24 01:41:53 -0500

    working on getting Yelp API set up for requests

--- yelp.py
+++ yelp.py
@@ -1,3 +1,12 @@
 import os
+import requests
 
-enVar = os.environ('YELP_API_KEY')
+enVar = os.environ.get('YELP_API_KEY')
+url = 'https://api.yelp.com/v3/businesses/'
+params = {'id':'WavvLdfdP6g8aZTtbBQHTw'}
+headers = {'Authorization': f'Bearer {enVar}'}
+
+data = requests.get(url, params=params, headers=headers).json()
+print(data)
+# name = data['name']
+# print(f'The name of the establishment is: {name}')
