02da2a371398495c2488abebc6a85ed8e45db8c4
Dani <danibarstad@gmail.com>
2020-06-01 21:21:40 -0500

    need make returned establishments into links

--- restroomrater/restroom_rater/views.py
+++ restroomrater/restroom_rater/views.py
@@ -10,6 +10,6 @@ def homepage(request):
     new_zip_form = get_zip()
     search_zip = request.GET.get('zip_code')
 
-    cities = yelp_api.get_name(search_zip)
+    establishments = yelp_api.get_name(search_zip)
 
-    return render(request, 'restroom_rater/homepage.html', { 'cities': cities, 'new_zip_form': new_zip_form})
+    return render(request, 'restroom_rater/homepage.html', { 'establishments': establishments, 'new_zip_form': new_zip_form})
