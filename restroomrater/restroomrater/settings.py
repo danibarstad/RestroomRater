13916a5876031a0cfe915163ce59c72acbdbe539
Dani <danibarstad@gmail.com>
2020-06-22 22:36:33 -0500

    true random secret generator

--- restroomrater/restroomrater/settings.py
+++ restroomrater/restroomrater/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import random
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'u_jsf5@$ilt)x-_wihpean#x93wg%uy260=3q(r%t0*@y1*3y2'
+# SECRET_KEY = 'u_jsf5@$ilt)x-_wihpean#x93wg%uy260=3q(r%t0*@y1*3y2'
+SECRET_KEY = ''.join([random.SystemRandom().choice('abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)') for i in range(50)])
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if os.getenv('GAE_INSTANCE'):
