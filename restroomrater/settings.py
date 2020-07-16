fb820251c7ee0e379cde548a8eec5e6ded7f3596
Dani <danibarstad@gmail.com>
2020-07-16 09:15:19 -0500

    connecting to postgres db

--- restroomrater/settings.py
+++ restroomrater/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import random
 import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -88,12 +89,12 @@ WSGI_APPLICATION = 'restroomrater.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 ''' FOR LOCAL '''
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 ''' FOR DEPLOYED '''
 # DATABASES = {
@@ -173,3 +174,5 @@ LOGOUT_REDIRECT_URL = 'homepage'
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
