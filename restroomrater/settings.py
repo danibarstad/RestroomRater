484f22c9d4c1d54d905e2bcb5e6412e4ce1bd8a1
Dani <danibarstad@gmail.com>
2020-08-09 17:35:48 -0500

    link font-weight edit

--- restroomrater/settings.py
+++ restroomrater/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
 }
 
 ''' FOR HEROKU '''
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Password validation
